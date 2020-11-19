5e9796dc6aa1bd536ea162fbc06b214689a3779c
MrVanek <vanekm@gilmour.org>
2020-11-19 15:50:30 -0500

    Rocket ship on load screen working

---
+++ MoonShot/Assets/Scripts/rocketShipSctipt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocketShipSctipt : MonoBehaviour
+{
+
+    float rocketSpeed = 40;
+    float rocketRotation = 30;
+
+    public Transform waypoint;
+    private Transform[] points;
+    private int waypointHeading = 0;
+    private Transform headedTowards;
+    private bool stopShip = false;
+    private Vector3 initialPosition;
+
+
+    private void Awake()
+    {
+
+    }
+    
+    void Start()
+    {
+        initialPosition = transform.position;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(transform.position, waypoint.position) < .5f)
+        {
+            GetNextWaypoint();
+        }
+
+        transform.LookAt (waypoint.position);
+        transform.position = Vector3.MoveTowards(transform.position, waypoint.position, rocketSpeed * Time.deltaTime);
+    }
+
+    private void GetNextWaypoint()
+    {
+        Debug.Log("Resetting");
+        transform.position = initialPosition;
+    }
+}
